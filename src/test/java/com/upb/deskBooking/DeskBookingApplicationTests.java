cf0b97c4dc16159924a007d5391e6f1c95f527a9
Busu <gabriel.busuioc1204@stud.acs.upb.ro>
2022-12-03 18:26:38 +0200

    Initial commit

---
+++ src/test/java/com/upb/deskBooking/DeskBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upb.deskBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
